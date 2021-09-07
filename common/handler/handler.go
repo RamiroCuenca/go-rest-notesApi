276bdc209f219e4064b1fca51f175df93b104fb7
Ramiro Cuenca Salinas <ramicuenca00@gmail.com>
2021-09-07 11:44:00 -0300

    Created common handlers

---
+++ common/handler/handler.go
@@ -0,0 +1,24 @@
+package handler
+
+import (
+	"net/http"
+)
+
+// Common function wich will be used to send responses for correct http requests
+func SendResponse(w http.ResponseWriter, status int, data []byte) {
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	w.Write(data)
+
+}
+
+// Common function wich will be used to send errors for incorrect http requests
+func SendError(w http.ResponseWriter, status int) {
+
+	data := []byte{}
+
+	w.Header().Set("Content-Type", "application/")
+	w.WriteHeader(status)
+	w.Write(data)
+}
