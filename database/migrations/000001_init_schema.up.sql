192c83dfd30908d5c4740eb7789d3a7f7dddc060
Ramiro Cuenca Salinas <ramicuenca00@gmail.com>
2021-09-08 10:30:49 -0300

    Created migrations files

---
+++ database/migrations/000001_init_schema.up.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS notes (
+    id SERIAL,
+    owner_name VARCHAR(55) NOT NULL,
+    title VARCHAR(100) NOT NULL,
+    details VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT now(),
+    updated_at TIMESTAMP,
+    CONSTRAINT notes_id_pk PRIMARY KEY (id)
+);
