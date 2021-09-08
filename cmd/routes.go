095c0c9ec01a2a4a8dac8c609323562c5761c5c1
Ramiro Cuenca Salinas <ramicuenca00@gmail.com>
2021-09-08 10:55:11 -0300

    Created routes

---
+++ cmd/routes.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+)
+
+func Routes() *chi.Mux {
+	// Create a new multiplexer
+	r := chi.NewMux()
+
+	// Use logger middleware from chi
+	r.Use(middleware.Logger)
+
+	// Handlers
+	r.Post("/api/v1/notes/create", nil)
+	r.Get("/api/v1/notes/readbyid/{id}", nil)
+	r.Get("/api/v1/notes/readall", nil)
+	r.Put("/api/v1/notes/update", nil)
+	r.Delete("/api/v1/notes/delete/{id}", nil)
+
+	return r
+}
