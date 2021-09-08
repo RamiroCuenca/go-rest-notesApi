bc86aa86d05acc0e6a9965a1f12256e77c515128
Ramiro Cuenca Salinas <ramicuenca00@gmail.com>
2021-09-08 10:14:18 -0300

    Inited database connection on main package

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,14 @@
 package main
 
-import "github.com/RamiroCuenca/go-rest-notesApi/common/logger"
+import (
+	"github.com/RamiroCuenca/go-rest-notesApi/common/logger"
+	"github.com/RamiroCuenca/go-rest-notesApi/database/connection"
+)
 
 func main() {
 	// Init Zap logger so that we can use it all over the app
 	logger.InitZapLogger()
+
+	// Init postgres database
+	connection.NewPostgresClient()
 }
