6aada0a2273c27fd6471b0ed1578044c359b4e1d
Ramiro Cuenca Salinas <ramicuenca00@gmail.com>
2021-09-09 16:40:48 -0300

    Created a controller that allow as fetch all the notes

--- notes/controllers/controllers.go
+++ notes/controllers/controllers.go
@@ -9,6 +9,7 @@ import (
 	"github.com/RamiroCuenca/go-rest-notesApi/common/logger"
 	"github.com/RamiroCuenca/go-rest-notesApi/database/connection"
 	"github.com/RamiroCuenca/go-rest-notesApi/notes/models"
+	"github.com/lib/pq"
 )
 
 // This handler creates a new note
@@ -73,7 +74,96 @@ func NotesCreate(w http.ResponseWriter, r *http.Request) {
 
 	// 8° Send the response
 	handler.SendResponse(w, http.StatusCreated, json)
+}
 
+// This handler fetch all notes
+func NotesGetAll(w http.ResponseWriter, r *http.Request) {
+	// 1° Create the sql statement and prepare null fields
+	q := `SELECT * FROM notes`
+
+	// 2° Open DB connection and start a transaction
+	db := connection.NewPostgresClient()
+
+	tx, err := db.Begin()
+	if err != nil {
+		logger.Log().Infof("Error starting transaction: %v", err)
+		handler.SendError(w, 500) // Internal Server Error
+		return
+	}
+
+	// 3° Prepare the transaction
+	stmt, err := tx.Prepare(q)
+	if err != nil {
+		logger.Log().Infof("Error preparing transaction: %v", err)
+		tx.Rollback()
+		handler.SendError(w, 500) // Internal Server Error
+		return
+	}
+	defer stmt.Close()
+
+	// 4° Execute the query and assign the returned id to the Note object
+	// We will use QueryRow because the exec method returns two methods that are
+	// not compatible with psql!
+	rows, err := stmt.Query() // Query() return Rows object
+	if err != nil {
+		logger.Log().Infof("Error executing query: %v", err)
+		tx.Rollback()
+		handler.SendError(w, 500) // Internal Server Error
+		return
+	}
+	defer rows.Close() // Close Rows object
+
+	// 5° Create an array that will hold the results
+	// and go over the rows object and assign each value to
+	// a Note object and then append it to the notesArr
+	// Also look that we handle the possible null values!
+	notesArr := []models.Note{}
+
+	for rows.Next() {
+
+		n := models.Note{}
+		nullDetail := sql.NullString{}
+		nullUpdateAt := pq.NullTime{}
+
+		err := rows.Scan(
+			&n.ID,
+			&n.OwnerName,
+			&n.Title,
+			&nullDetail,
+			&n.CreatedAt,
+			&nullUpdateAt,
+		)
+
+		if err != nil {
+			logger.Log().Infof("Error scaning the received rows: %v", err)
+			tx.Rollback()
+			handler.SendError(w, 500) // Internal Server Error
+			return
+		}
+
+		n.Details = nullDetail.String
+		n.UpdatedAt = nullUpdateAt.Time
+
+		notesArr = append(notesArr, n)
+
+	}
+
+	// 6° As there are no errors, commit the transaction
+	tx.Commit()
+	logger.Log().Infof("Notes fetched successfully! :)")
+
+	// 7° Encode the Note into a JSON object
+	// There a many ways we can do this, but i choose to use the Marshal
+	// method as it organices for us the Json an it looks preety.
+	json, _ := json.Marshal(notesArr)
+
+	// Otherway, we can do this:
+	// _ = json.NewEncoder(w).Encode(notesArr)
+	// w.Header().Set("Content-Type", "application/json")
+	// w.WriteHeader(http.StatusOK)
+
+	// 8° Send the response
+	handler.SendResponse(w, http.StatusOK, json)
 }
 
 // This function manages the null string values
@@ -86,43 +176,3 @@ func stringToNull(s string) sql.NullString {
 
 	return null
 }
-
-// func (db *Database) NotesGetAll() ([]models.Note, error) {
-// 	// 1° Prepare the query that will be executed
-// 	q := `SELECT id, owner_name, title, details
-// 		created_at, updated_at FROM notes`
-
-// 	// Be prepared if details or updated at are empty
-// 	// detailsNull := sql.NullString{}
-
-// 	// 2° Open a transaction
-// 	tx, err := db.Begin()
-// 	if err != nil {
-// 		logger.Log().Infof("There was an error beggining the tx. Error: %v", err)
-// 		return nil, err
-// 	}
-
-// 	// 3° Prepare the query
-// 	stmt, err := tx.Prepare(q)
-// 	if err != nil {
-// 		logger.Log().Infof("There was an error preparing the sql statement. Error: %v", err)
-// 		tx.Rollback()
-// 		return nil, err
-// 	}
-// 	defer stmt.Close()
-
-// 	// 4° Execute the query. Send the parameters
-// 	// We scan it and assign the returned id to the object
-// 	// StringNull == note.details
-// 	// err = stmt.QueryRow(note.OwnerName, note.Title, stringNull).Scan(&note.ID)
-// 	if err != nil {
-// 		logger.Log().Infof("There was an error executing the sql statement. Error: %v", err)
-// 		tx.Rollback()
-// 		return nil, err
-// 	}
-
-// 	tx.Commit()
-// 	logger.Log().Info("Note created successfully :)")
-
-// 	return nil, nil
-// }
